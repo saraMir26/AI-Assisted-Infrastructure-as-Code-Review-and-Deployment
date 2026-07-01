91fc1dd98abf2ec9ca8d0acd710584de5846c6c8
saraMir26 <mirz0014@algonquinlive.com>
2026-06-30 21:41:51 -0400

    Refactor Terraform demo into flawed and fixed configurations

--- terraform/flawed/main.tf
+++ terraform/flawed/main.tf
@@ -1,33 +1,28 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "myrg123"
-  location = "Canada Central"
+  location = var.location
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "cloudnovapublicstore"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-  # Intentional issue: public access enabled for demo
-  public_network_access_enabled = true
-  allow_nested_items_to_be_public = true
+resource "azurerm_subnet" "subnet" {
+  name                 = "default-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "my-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_security_group" "nsg" {
@@ -48,7 +43,47 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-# Intentional cost issue: oversized VM variable for a test environment
-variable "vm_size" {
-  default = "Standard_D8s_v3"
+resource "azurerm_network_interface" "nic" {
+  name                = "my-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                            = "cloudnovapublicstore"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = true
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "myvm123"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = "adminuser"
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
